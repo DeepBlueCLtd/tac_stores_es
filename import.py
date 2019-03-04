473b7d794f6ecb4460438b38b614361d8644b483
alex <alex@diplux.com>
2019-03-04 19:44:37 +0600

    DSF file handling added

--- import.py
+++ import.py
@@ -2,7 +2,7 @@ import datetime
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
-file_paths_array = ["data/track_one.rep", "data/track_two.rep"]
+file_paths_array = ["data/track_one.rep", "data/track_two.rep", "data/sen_frig_sensor.dsf"]
 es = Elasticsearch()
 
 elastic_data = []
@@ -11,15 +11,20 @@ col_names = ["date/time", "yymmdd", "hummus.SSS", "platform", "colour", "positio
 
 
 def format_position(info_arr):
-    # degrees + minutes / 60 + seconds / (60 * 60)
-    i_lat = int(float(info_arr[4])) + (int(float(info_arr[5])) / 60) + (int(float(info_arr[6])) / (60 * 60))
-    i_lon = int(float(info_arr[8])) + (int(float(info_arr[9])) / 60) + (int(float(info_arr[10])) / (60 * 60))
+    if not type(info_arr[4]) is str and not type(info_arr[5]) is str and not type(info_arr[6]) is str and not type(info_arr[8]) is str and not type(info_arr[9]) is str and not type(info_arr[10]) is str:
+        # degrees + minutes / 60 + seconds / (60 * 60)
+        i_lat = int(float(info_arr[4])) + (int(float(info_arr[5])) / 60) + (int(float(info_arr[6])) / (60 * 60))
+        i_lon = int(float(info_arr[8])) + (int(float(info_arr[9])) / 60) + (int(float(info_arr[10])) / (60 * 60))
 
-    if info_arr[7] == 'S':
-        i_lat = -1 * i_lat
+        if info_arr[7] == 'S':
+            i_lat = -1 * i_lat
 
-    if info_arr[11] == 'W':
-        i_lon = -1 * i_lon
+        if info_arr[11] == 'W':
+            i_lon = -1 * i_lon
+
+    else:
+        i_lat = None
+        i_lon = None
 
     return i_lat, i_lon
 
@@ -41,6 +46,14 @@ def run_files_import(file_paths):
                 # splitting the line off
                 info = item.split()
 
+                if not info:
+                    continue
+
+                if "SENSOR" in info[0]:
+                    tuple_tmp = (info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9], info[10])
+                    new_item = ' '.join(tuple_tmp)
+                    info = new_item.split()
+
                 # time field
                 date_str = info[0] + " " + info[1]
                 date_time_obj = datetime.datetime.strptime(date_str, '%y%m%d %H%M%S.%f')
@@ -53,13 +66,22 @@ def run_files_import(file_paths):
                 elastic_entry["color"] = info[3]
 
                 # course field
-                elastic_entry["course"] = info[12]
+                if 12 in info:
+                    elastic_entry["course"] = info[12]
+                else:
+                    elastic_entry["course"] = None
 
                 # speed field
-                elastic_entry["speed"] = info[13]
+                if 13 in info:
+                    elastic_entry["speed"] = info[13]
+                else:
+                    elastic_entry["speed"] = None
 
                 # depth field
-                elastic_entry["depth"] = info[14]
+                if 14 in info:
+                    elastic_entry["depth"] = info[14]
+                else:
+                    elastic_entry["depth"] = None
 
                 # serial field
                 elastic_entry["serial"] = "EX_ALPHA"
@@ -76,6 +98,9 @@ def run_files_import(file_paths):
                 # position field
                 lat, lon = format_position(info)
                 elastic_entry["location"] = {"lat": lat, "lon": lon}
+
+                #custom type field
+                elastic_entry["meas_type"] = "default"
 
                 elastic_data.append(elastic_entry)
 
