622903f86b5a61e087ea68f3bf3ea8951f2b6084
alex <alex@diplux.com>
2019-03-06 19:07:36 +0600

    Try/Except was removed from code

--- import.py
+++ import.py
@@ -5,26 +5,29 @@ import datetime
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 
-file_paths_array = ["data/track_one.rep", "data/track_two.rep", "data/sen_frig_sensor.dsf"]
-es = Elasticsearch()
+file_paths_array = []
+PATHS_OF_FILES = "list_of_filenames.txt"
 
+es = Elasticsearch()
 elastic_data = []
 
 
+def read_file(source_filename):
+    f = open(source_filename, "r")
+    for filepath in f:
+        file_paths_array.append(filepath.strip())
+
 def format_position(info_arr):
-    try:
-        # degrees + minutes / 60 + seconds / (60 * 60)
-        i_lat = int(float(info_arr[4])) + (int(float(info_arr[5])) / 60) + (int(float(info_arr[6])) / (60 * 60))
-        i_lon = int(float(info_arr[8])) + (int(float(info_arr[9])) / 60) + (int(float(info_arr[10])) / (60 * 60))
+    # degrees + minutes / 60 + seconds / (60 * 60)
+    i_lat = int(float(info_arr[4])) + (int(float(info_arr[5])) / 60) + (int(float(info_arr[6])) / (60 * 60))
+    i_lon = int(float(info_arr[8])) + (int(float(info_arr[9])) / 60) + (int(float(info_arr[10])) / (60 * 60))
 
-        if info_arr[7] == 'S':
-            i_lat = -1 * i_lat
+    if info_arr[7] == 'S':
+        i_lat = -1 * i_lat
 
-        if info_arr[11] == 'W':
-            i_lon = -1 * i_lon
-    except Exception:
-        i_lat = None
-        i_lon = None
+    if info_arr[11] == 'W':
+        i_lon = -1 * i_lon
+
 
     return i_lat, i_lon
 
@@ -37,6 +40,8 @@ def run_files_import(file_paths):
             es_index = 'states'
         elif '.dsf' in path:
             es_index = 'contacts'
+        else:
+            continue
 
         with open(path, "r") as f:
             measurements = [line.strip() for line in f.readlines()]
@@ -55,11 +60,18 @@ def run_files_import(file_paths):
                 if not info:
                     continue
 
+                # Checking the type of file and using of needed logic
                 if "SENSOR" in info[0]:
                     tuple_tmp = (
                         info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9], info[10])
                     new_item = ' '.join(tuple_tmp)
                     info = new_item.split()
+                    # position field
+                    elastic_entry["location"] = {"lat": None, "lon": None}
+                else:
+                    # position field
+                    lat, lon = format_position(info)
+                    elastic_entry["location"] = {"lat": lat, "lon": lon}
 
                 # time field
                 date_str = info[0] + " " + info[1]
@@ -102,10 +114,6 @@ def run_files_import(file_paths):
                 # privacy field
                 elastic_entry["privacy"] = "public"
 
-                # position field
-                lat, lon = format_position(info)
-                elastic_entry["location"] = {"lat": lat, "lon": lon}
-
                 # custom type field
                 elastic_entry["meas_type"] = "default"
 
@@ -127,6 +135,8 @@ def prepare_data(input_data=[]):
         }
 
 
+print("Reading list of files")
+read_file(PATHS_OF_FILES)
 print("Preparing data")
 run_files_import(file_paths_array)
 print("Transferring the data")
