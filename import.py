4462326af65a6a7778e30e5bd018135f833e3aed
alex <alex@diplux.com>
2019-03-08 00:20:12 +0600

    exception handling while import goes

--- import.py
+++ import.py
@@ -14,6 +14,7 @@ PATHS_OF_FILES = "list_of_filenames.txt"
 es = Elasticsearch()
 elastic_data = []
 
+error_log_file = open("error_log.txt", "w+")
 
 def read_file(source_filename):
     f = open(source_filename, "r")
@@ -55,92 +56,99 @@ def date_time_format(date_str):
 
 def run_files_import(file_paths):
     for path in file_paths:
-        print(path)
-        es_index = ''
-        if '.rep' in path:
-            es_index = 'states'
-        elif '.dsf' in path:
-            es_index = 'contacts'
-        else:
-            continue
+        try:
+            print(path)
+            es_index = ''
+            if '.rep' in path:
+                es_index = 'states'
+            elif '.dsf' in path:
+                es_index = 'contacts'
+            else:
+                continue
 
-        with open(path, "r") as f:
-            measurements = [line.strip() for line in f.readlines()]
+            with open(path, "r") as f:
+                measurements = [line.strip() for line in f.readlines()]
 
-            for ind, item in enumerate(measurements):
+                for ind, item in enumerate(measurements):
 
-                # single object for each record
-                elastic_entry = {}
-                # splitting the line off
-                info = item.split()
+                    # single object for each record
+                    elastic_entry = {}
+                    # splitting the line off
+                    info = item.split()
 
-                if not info:
-                    continue
+                    if not info:
+                        continue
 
-                if ';;' in info[0] or ';TIMETEXT:' in info[0] or ';PERIODTEXT:' in info[0] or ';TEXT:' in info[0]:
-                    continue
+                    if ';;' in info[0] or ';TIMETEXT:' in info[0] or ';PERIODTEXT:' in info[0] or ';TEXT:' in info[0]:
+                        continue
 
-                # Checking the type of file and using of needed logic
-                if "SENSOR" in info[0]:
-                    tuple_tmp = (
-                        info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9], info[10])
-                    new_item = ' '.join(tuple_tmp)
-                    info = new_item.split()
-                    # position field
-                    elastic_entry["location"] = {"lat": None, "lon": None}
-                else:
-                    # position field
-                    lat, lon = format_position(info)
-                    elastic_entry["location"] = {"lat": lat, "lon": lon}
+                    # Checking the type of file and using of needed logic
+                    if "SENSOR" in info[0]:
+                        tuple_tmp = (
+                            info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9], info[10])
+                        new_item = ' '.join(tuple_tmp)
+                        info = new_item.split()
+                        # position field
+                        elastic_entry["location"] = {"lat": None, "lon": None}
+                    else:
+                        # position field
+                        lat, lon = format_position(info)
+                        elastic_entry["location"] = {"lat": lat, "lon": lon}
 
-                # time field
-                date_str = info[0] + " " + info[1]
-                date_time_obj = date_time_format(date_str)
-                elastic_entry["time"] = date_time_obj
+                    # time field
+                    date_str = info[0] + " " + info[1]
+                    date_time_obj = date_time_format(date_str)
+                    elastic_entry["time"] = date_time_obj
 
-                # platform field
-                elastic_entry["platform"] = info[2]
+                    # platform field
+                    elastic_entry["platform"] = info[2]
 
-                # color field
-                elastic_entry["color"] = info[3]
+                    # color field
+                    elastic_entry["color"] = info[3]
 
-                # course field
-                if 12 in info:
-                    elastic_entry["course"] = info[12]
-                else:
-                    elastic_entry["course"] = None
+                    # course field
+                    if 12 in info:
+                        elastic_entry["course"] = info[12]
+                    else:
+                        elastic_entry["course"] = None
 
-                # speed field
-                if 13 in info:
-                    elastic_entry["speed"] = info[13]
-                else:
-                    elastic_entry["speed"] = None
+                    # speed field
+                    if 13 in info:
+                        elastic_entry["speed"] = info[13]
+                    else:
+                        elastic_entry["speed"] = None
 
-                # depth field
-                if 14 in info:
-                    elastic_entry["depth"] = info[14]
-                else:
-                    elastic_entry["depth"] = None
+                    # depth field
+                    if 14 in info:
+                        elastic_entry["depth"] = info[14]
+                    else:
+                        elastic_entry["depth"] = None
 
-                # serial field
-                elastic_entry["serial"] = "EX_ALPHA"
+                    # serial field
+                    elastic_entry["serial"] = "EX_ALPHA"
 
-                # sensor field
-                elastic_entry["sensor"] = "GPS"
+                    # sensor field
+                    elastic_entry["sensor"] = "GPS"
 
-                # source field
-                elastic_entry["source"] = "CD_123"
+                    # source field
+                    elastic_entry["source"] = "CD_123"
 
-                # privacy field
-                elastic_entry["privacy"] = "public"
+                    # privacy field
+                    elastic_entry["privacy"] = "public"
 
-                # custom type field
-                elastic_entry["meas_type"] = "default"
+                    # custom type field
+                    elastic_entry["meas_type"] = "default"
 
-                # es index name
-                elastic_entry['es_index'] = es_index
+                    # es index name
+                    elastic_entry['es_index'] = es_index
 
-                elastic_data.append(elastic_entry)
+                    elastic_data.append(elastic_entry)
+        except ValueError as e:
+            print(ValueError)
+            import datetime
+            now = datetime.datetime.now()
+            error_log_file.write('File path: ' + path + '; Error: ' + str(e) + '; Time: ' + now.isoformat() + '\n')
+
 
 
 def prepare_data(input_data=[]):
