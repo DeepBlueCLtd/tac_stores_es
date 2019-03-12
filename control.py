1438079e311d8a133d738413cd3e47457c3a0dfc
alex <alex@diplux.com>
2019-03-12 15:46:17 +0600

    bugs fixed, dynamic variables require previous definition

--- control.py
+++ control.py
@@ -3,5 +3,4 @@ import files_reader
 # path to the data directory
 SOURCE_PATH = "data/"
 
-rep, dsf = files_reader.get_files(SOURCE_PATH, ["rep", "dsf"])
-
+dsf, rep = files_reader.get_files(SOURCE_PATH, ["dsf", "rep"])
--- files_reader.py
+++ files_reader.py
@@ -6,8 +6,7 @@ dest_dir = "res_files/"
 
 # files in the data folder
 all_filenames_data = []
-dsf_files = []
-rep_files = []
+
 
 # reqursive function for handling folder of data
 def recursive_read(path, possible_types=[]):
@@ -20,8 +19,10 @@ def recursive_read(path, possible_types=[]):
                 all_filenames_data.append(path + filename)
                 # checking file types
                 for f_type in possible_types:
-                    if filename.endswith("."+f_type):
+
+                    if filename.endswith("." + f_type):
                         globals()[f_type + "_files"].append(path + filename)
+
 
         if dirnames:
             # go through directories
@@ -32,6 +33,9 @@ def recursive_read(path, possible_types=[]):
 
 
 def get_files(path, possible_types=[]):
+    for p_type in possible_types:
+        globals()[p_type + "_files"] = []
+
     recursive_read(path, possible_types)
 
     if not os.path.exists(dest_dir):
@@ -42,17 +46,5 @@ def get_files(path, possible_types=[]):
     for filepath in all_filenames_data:
         file.write(filepath + '\n')
     file.close()
-
-    # save rep files
-    # file = open(dest_dir + "list_rep_filenames.txt", "w")
-    # for filepath in rep_files:
-    #     file.write(filepath + '\n')
-    # file.close()
-    #
-    # # save dsf files
-    # file = open("res_files/list_dsf_filenames.txt", "w")
-    # for filepath in dsf_files:
-    #     file.write(filepath + '\n')
-    # file.close()
 
     return (globals()[p_type + "_files"] for p_type in possible_types)
